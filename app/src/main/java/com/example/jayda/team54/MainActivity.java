249a0d788d6d510b209987a77c91709fe26628a6
Jayaram Dargula <jaydargula@gmail.com>
2017-02-12 20:45:11 -0500

    added progress dialog

--- app/src/main/java/com/example/jayda/team54/MainActivity.java
+++ app/src/main/java/com/example/jayda/team54/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.jayda.team54;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,10 +18,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editTextPassword;
     private TextView textViewSignIn;
 
+    private ProgressDialog progressDialog;
+
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(this);
 
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
@@ -38,12 +46,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (TextUtils.isEmpty(email)) {
             //email is empty
             Toast.makeText(this, "Please enter your email address", Toast.LENGTH_SHORT);
+            //stopping function execution
             return;
         }
 
         if (TextUtils.isEmpty(password)) {
             //password is empty
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT);
+            //stopping function execution
+            return;
         }
+        //if validated, register user
+
+        progressDialog.setMessage("One moment please...");
+        progressDialog.show();
+
+
+
     }
 
     @Override
