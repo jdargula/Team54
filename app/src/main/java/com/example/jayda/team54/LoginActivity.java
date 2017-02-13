3b11207bd3105702f03d971c05d1d898f20736f5
Jayaram Dargula <jaydargula@gmail.com>
2017-02-12 23:44:08 -0500

    implementing existing user login

--- app/src/main/java/com/example/jayda/team54/LoginActivity.java
+++ app/src/main/java/com/example/jayda/team54/LoginActivity.java
@@ -1,13 +1,51 @@
 package com.example.jayda.team54;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ViewStubCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // initialize widgets
+    private Button buttonSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // get data from gui
+        buttonSignIn = (Button) findViewById(R.id.buttonSignIn);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignUp = (TextView) findViewById(R.id.textViewSignUp);
+
+        buttonSignIn.setOnClickListener(this);
+        textViewSignUp.setOnClickListener(this);
+
+
     }
+
+    public void onClick(View view) {
+        if (view == buttonSignIn) {
+            userLogin();
+        }
+
+        if (view == textViewSignUp) {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+
 }
