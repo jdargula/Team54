a1e995e0d8c8a39b077b7753771ac2987ef852aa
Jayaram Dargula <jaydargula@gmail.com>
2017-02-12 20:31:28 -0500

    added references to login screen widgets

--- app/src/main/java/com/example/jayda/team54/MainActivity.java
+++ app/src/main/java/com/example/jayda/team54/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.jayda.team54;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewSignIn = (TextView) findViewById(R.id.textViewSignIn);
     }
 }
