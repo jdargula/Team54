4a49681468ede48a8a1fc1f53a66a4b5b12cdf4e
Jayaram Dargula <jaydargula@gmail.com>
2017-02-17 03:03:21 -0500

    added functionality to editing user info

--- app/src/main/java/com/example/jayda/team54/ApplicationActivity.java
+++ app/src/main/java/com/example/jayda/team54/ApplicationActivity.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ApplicationActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,7 +23,8 @@ public class ApplicationActivity extends AppCompatActivity implements View.OnCli
 
     private DatabaseReference databaseReference;
 
-    
+    private EditText editTextName, editTextAddress;
+    private Button buttonSaveInformation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +39,35 @@ public class ApplicationActivity extends AppCompatActivity implements View.OnCli
             startActivity(new Intent(this, LoginActivity.class));
         }
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        //assign profile view widgets
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        buttonSaveInformation = (Button) findViewById(R.id.buttonSaveInformation);
+
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
         textViewUserEmail = (TextView) findViewById(R.id.textViewUserEmail);
         textViewUserEmail.setText("Welcome, " + user.getEmail());
         buttonLogout = (Button) findViewById(R.id.buttonLogout);
 
+        // add listeners to buttons
         buttonLogout.setOnClickListener(this);
+        buttonSaveInformation.setOnClickListener(this);
+    }
 
+    private void saveUserInformation() {
+        String name = editTextName.getText().toString().trim();
+        String address = editTextAddress.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(name, address);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        databaseReference.child(user.getUid()).setValue(userInformation);
+
+        Toast.makeText(this, "Information Saved", Toast.LENGTH_LONG);
     }
 
     @Override
---
+++ app/src/main/java/com/example/jayda/team54/UserInformation.java
@@ -0,0 +1,19 @@
+package com.example.jayda.team54;
+
+/**
+ * Created by jayda on 2/17/2017.
+ */
+
+public class UserInformation {
+
+    public UserInformation() {}
+
+    public UserInformation(String n, String a) {
+        name = n;
+        address = a;
+    }
+
+    public String name;
+    public String address;
+
+}
